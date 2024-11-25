668257ee19accc24dfd4a433aac0a1c34edcd022
shahbajsingh <ss4589@njit.edu>
2024-11-25 02:01:21 -0500

    migrate

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Social = () => {
+    return (
+        <div className="home__social">
+            <a href="https://instagram.com/shahbajpannu" className="home__social-icon" target='_blank'>
+                <i class="uil uil-instagram"></i>
+            </a>
+
+            <a href="https://www.linkedin.com/in/shahbaj-singh/" className="home__social-icon" target='_blank'>
+                <i class="uil uil-linkedin"></i>
+            </a>
+
+            <a href="https://github.com/shahbajsingh" className="home__social-icon" target='_blank'>
+                <i class="uil uil-github"></i>
+            </a>
+        </div>
+    )
+}
+
+export default Social
---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,30 @@
+import Image1 from "../../assets/testimonial1.jpg";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.jpg";
+
+export const Data = [
+    {
+        id: 1,
+        image: Image1,
+        title: "Deepak K.",
+        subtitle: "KP³ Co-Founder",
+        description:
+        "Building KP³ with Shahbaj has been a pivotal experience in my career as an entrepreneur, consultant, and developer.",
+    },
+    {
+        id: 2,
+        image: Image2,
+        title: "Archie M.",
+        subtitle: "Medical Student",
+        description:
+        "Being a medical professional, I always trust Shahbaj's expertise to guide me through the digital world.",
+    },
+    {
+        id: 3,
+        image: Image3,
+        title: "Jasmit B.",
+        subtitle: "Software Developer",
+        description:
+        "Working with Shahbaj is always a pleasure– his dedication to development is second to none.",
+    },
+];
